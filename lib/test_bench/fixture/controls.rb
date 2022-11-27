535b878fd35a08ae561b63ed48460db44af5551f
Nathan Ladd <nathanladd@gmail.com>
2022-11-26 19:43:14 -0600

    Fixture: Comment

--- lib/test_bench/fixture/controls.rb
+++ lib/test_bench/fixture/controls.rb
@@ -4,3 +4,5 @@ require 'test_bench/fixture/controls/fixture'
 require 'test_bench/fixture/controls/fixture/class'
 require 'test_bench/fixture/controls/fixture/object/modules'
 require 'test_bench/fixture/controls/fixture/object'
+
+require 'test_bench/fixture/controls/output'
