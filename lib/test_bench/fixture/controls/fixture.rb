f06459a50bdba64bdfc935c8692af6d89cedaf11
Nathan Ladd <nathanladd@gmail.com>
2022-11-26 19:43:14 -0600

    Fixture: Comment

--- lib/test_bench/fixture/controls/fixture.rb
+++ lib/test_bench/fixture/controls/fixture.rb
@@ -6,6 +6,18 @@ module TestBench
           Example.new
         end
 
+        def self.call(output=nil, &block)
+          output ||= Output.example
+
+          fixture = Example.new
+
+          fixture.test_session.telemetry.register(output)
+
+          block.(fixture)
+
+          fixture
+        end
+
         class Example
           include TestBench::Fixture
         end
