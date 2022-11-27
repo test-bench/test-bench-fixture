81d432e449c54baa31ab906e3e402b69afd762f5
Nathan Ladd <nathanladd@gmail.com>
2022-11-26 19:43:14 -0600

    Fixture: Comment

--- lib/test_bench/fixture/fixture.rb
+++ lib/test_bench/fixture/fixture.rb
@@ -1,9 +1,54 @@
 module TestBench
   module Fixture
+    include TestBench::Session::Events
+
     def test_session
       @test_session ||= Session::Substitute.build
     end
     attr_writer :test_session
+
+    def comment(...)
+      Fixture.comment(test_session.telemetry, Commented, ...)
+    end
+
+    def self.comment(telemetry, event_class, text, *additional_texts, heading: nil, quote: nil)
+      texts = [text, *additional_texts]
+
+      if quote.nil?
+        quote = texts.last.end_with?("\n")
+      end
+
+      if quote
+        if heading.nil?
+          heading = !text.end_with?("\n")
+        end
+      end
+
+      if heading
+        heading = texts.shift
+      else
+        heading = nil
+      end
+
+      if quote
+        text = texts.join
+      else
+        text = texts.first
+      end
+
+      event = event_class.new
+      event.text = text
+      event.quote = quote
+      event.heading = heading
+
+      telemetry.record(event)
+
+      if not quote
+        texts[1..-1].each do |text|
+          comment(telemetry, event_class, text, heading: false, quote: false)
+        end
+      end
+    end
 
     def self.call(fixture_class_or_object, ...)
       if fixture_class_or_object.instance_of?(Class)
