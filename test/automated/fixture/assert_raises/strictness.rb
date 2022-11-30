8e9963e2a9609d6735991050874a0278a11200ad
Nathan Ladd <nathanladd@gmail.com>
2022-11-30 11:59:38 -0600

    Fixture: Assert Raises

---
+++ test/automated/fixture/assert_raises/strictness.rb
@@ -0,0 +1,49 @@
+require_relative '../../automated_init'
+
+context "Fixture" do
+  context "Assert Raises" do
+    context "Strictness" do
+      exception = Controls::Exception.example
+
+      exception_class = Controls::Exception::Example.superclass
+
+      context "Strict" do
+        strict = true
+
+        context "Subclass of Given Exception Raised" do
+          fixture = Controls::Fixture.example
+
+          begin
+            fixture.assert_raises(exception_class, strict:) do
+              raise exception
+            end
+          rescue exception.class => raised_exception
+          end
+
+          test "Not rescued" do
+            refute(raised_exception.nil?)
+          end
+        end
+      end
+
+      context "Non-Strict" do
+        strict = false
+
+        context "Subclass of Given Exception Raised" do
+          fixture = Controls::Fixture.example
+
+          begin
+            fixture.assert_raises(exception_class, strict:) do
+              raise exception
+            end
+          rescue exception.class => raised_exception
+          end
+
+          test "Rescued" do
+            assert(raised_exception.nil?)
+          end
+        end
+      end
+    end
+  end
+end
