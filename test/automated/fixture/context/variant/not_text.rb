6aaa5a3a28371ebffa7acf8061f7f86df6cc1126
Nathan Ladd <nathanladd@gmail.com>
2022-12-01 12:11:27 -0600

    Fixture: Context

---
+++ test/automated/fixture/context/variant/not_text.rb
@@ -0,0 +1,21 @@
+require_relative '../../../automated_init'
+
+context "Fixture" do
+  context "Context" do
+    context "Variant" do
+      context "Not Text" do
+        title = Object.new
+
+        fixture = Controls::Fixture.example
+
+        test "Is a NoMethodError" do
+          assert_raises(NoMethodError, "undefined method `to_str' for #{title.inspect}") do
+            fixture.context! title do
+              #
+            end
+          end
+        end
+      end
+    end
+  end
+end
