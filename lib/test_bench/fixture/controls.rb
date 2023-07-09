d5fe7d5acfe6dafb20c0b607c8cac5fbc8a7df2c
Nathan Ladd <nathanladd@gmail.com>
2023-07-09 12:43:04 -0500

    Actuate (Object): Included Modules

--- lib/test_bench/fixture/controls.rb
+++ lib/test_bench/fixture/controls.rb
@@ -2,3 +2,5 @@ require 'test_bench/session/controls'
 
 require 'test_bench/fixture/controls/fixture'
 require 'test_bench/fixture/controls/fixture/class'
+require 'test_bench/fixture/controls/fixture/object/modules'
+require 'test_bench/fixture/controls/fixture/object'
