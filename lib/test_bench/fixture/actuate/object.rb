40f500dfa8a5f44677ef2abd5882a23ded170348
Nathan Ladd <nathanladd@gmail.com>
2023-07-09 12:50:52 -0500

    Actuate (Object): Fixture Module Predicate

--- lib/test_bench/fixture/actuate/object.rb
+++ lib/test_bench/fixture/actuate/object.rb
@@ -2,6 +2,20 @@ module TestBench
   module Fixture
     module Actuate
       module Object
+        def self.fixture_module?(object)
+          target = fixture_module_target(object)
+
+          target.const_defined?(:Fixture, false)
+        end
+
+        def self.fixture_module_target(object)
+          if object.is_a?(Module)
+            object
+          else
+            object.class
+          end
+        end
+
         def self.included_modules(object)
           modules = (object.class.included_modules | object.singleton_class.included_modules) - ::Object.included_modules
           modules.sort_by(&:name)
